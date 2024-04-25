c5435dcb085b0854c6374529d7546d20dbe80e19
Jolmoz <hayabusa000@hotmail.com>
2024-04-25 11:22:02 -0500

    init web app

---
+++ src/main/java/com/jolmoz/threelinewebapp/ThreeLineWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.jolmoz.threelinewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeLineWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreeLineWebAppApplication.class, args);
+	}
+
+}
