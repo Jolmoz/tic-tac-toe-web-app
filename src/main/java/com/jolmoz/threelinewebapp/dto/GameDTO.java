19fbae9d52b8bc151bf6987d296c1177a6a0a28b
Jolmoz <hayabusa000@hotmail.com>
2024-04-26 11:43:16 -0500

    add server dtos

--- src/main/java/com/jolmoz/threelinewebapp/dto/GameDTO.java
+++ src/main/java/com/jolmoz/threelinewebapp/dto/GameDTO.java
@@ -1,5 +1,93 @@
 package com.jolmoz.threelinewebapp.dto;
 
+import java.util.Date;
+
+import com.jolmoz.threelinewebapp.model.Game;
+import com.jolmoz.threelinewebapp.model.Game.BoardState;
+
 public class GameDTO {
-    
+    private long id;
+    private String gameName;
+    private String board;
+    private PlayerDTO playerX;
+    private PlayerDTO playerO;
+    private PlayerDTO playerWinner;
+    private BoardState boardState;
+    private Date lastSavedDate;
+
+    public GameDTO(Game game) {
+        this.id = game.getId();
+        this.gameName = game.getGameName();
+        this.board = game.getBoard();
+        this.playerX = new PlayerDTO(game.getPlayerX());
+        this.playerO = new PlayerDTO(game.getPlayerO());
+        this.playerWinner = new PlayerDTO(game.getPlayerWinner());
+        this.boardState = game.getBoardState();
+        this.lastSavedDate = game.getLastSavedDate();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public String getBoard() {
+        return board;
+    }
+
+    public void setBoard(String board) {
+        this.board = board;
+    }
+
+    public PlayerDTO getPlayerX() {
+        return playerX;
+    }
+
+    public void setPlayerX(PlayerDTO playerX) {
+        this.playerX = playerX;
+    }
+
+    public PlayerDTO getPlayerO() {
+        return playerO;
+    }
+
+    public void setPlayerO(PlayerDTO playerO) {
+        this.playerO = playerO;
+    }
+
+    public PlayerDTO getPlayerWinner() {
+        return playerWinner;
+    }
+
+    public void setPlayerWinner(PlayerDTO playerWinner) {
+        this.playerWinner = playerWinner;
+    }
+
+    public BoardState getBoardState() {
+        return boardState;
+    }
+
+    public void setBoardState(BoardState boardState) {
+        this.boardState = boardState;
+    }
+
+    public Date getLastSavedDate() {
+        return lastSavedDate;
+    }
+
+    public void setLastSavedDate(Date lastSavedDate) {
+        this.lastSavedDate = lastSavedDate;
+    }
+
 }
--- src/main/java/com/jolmoz/threelinewebapp/dto/PlayerDTO.java
+++ src/main/java/com/jolmoz/threelinewebapp/dto/PlayerDTO.java
@@ -1,5 +1,41 @@
 package com.jolmoz.threelinewebapp.dto;
 
+import com.jolmoz.threelinewebapp.model.Player;
+import com.jolmoz.threelinewebapp.model.Player.PlayerType;
+
 public class PlayerDTO {
-    
+    private long id;
+    private String playerName;
+    private PlayerType playerType;
+
+    public PlayerDTO(Player player) {
+        this.id = player.getId();
+        this.playerName = player.getPlayerName();
+        this.playerType = player.getPlayerType();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public PlayerType getPlayerType() {
+        return playerType;
+    }
+
+    public void setPlayerType(PlayerType playerType) {
+        this.playerType = playerType;
+    }
+
 }
